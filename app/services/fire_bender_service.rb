fe2b28d436d5b4735391ef5a1d3d3db1f05c623a
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-22 09:28:06 -0600

    Connect to api

--- app/services/fire_bender_service.rb
+++ app/services/fire_bender_service.rb
@@ -1,3 +1,15 @@
 class FireBenderService
+  def self.get_nation_info(fire_nation)
+    response = conn.get('api/v1/characters') do |f|
+      f.params['affiliation'] = fire_nation
+    end
+    # binding.pry
+    JSON.parse(response.body, symbolize_names: true)
+  end
+    
   
+  def self.conn 
+    url = 'https://last-airbender-api.herokuapp.com/'
+    Faraday.new(url: url)
+  end
 end
--- spec/services/fire_bender_spec.rb
+++ spec/services/fire_bender_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe FireBenderService do 
   it 'returns the data from the API' do 
-    fire_nation = FireBenderService.find_data
-
-    expect(fire_nation).to have_key([:allies])
-    expect(fire_nation).to have_key([:enemies])
-    expect(fire_nation).to have_key([:name])
-    expect(fire_nation).to have_key([:affiliation])
-    expect(fire_nation).to have_key([:photoUrl])
+    fire_nation = FireBenderService.get_nation_info('Fire Nation')
+    # binding.pry
+    data = fire_nation[0].each do |fire|
+      expect(fire_nation[0]).to have_key([:allies])
+      expect(fire_nation[0]).to have_key([:enemies])
+      expect(fire_nation[0]).to have_key([:name])
+      expect(fire_nation[0]).to have_key([:affiliation])
+      expect(fire_nation[0]).to have_key([:photoUrl])
+    end
   end
 end

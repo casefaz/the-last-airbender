2dd568a3b68b9f1e104b992af2fc0d9bc8beb6ab
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-22 09:41:02 -0600

    Add poros test

---
+++ spec/facades/fire_facade_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe FireFacade do 
+  it 'produces poros objects with service data' do 
+    
+  end
+end
---
+++ spec/poros/fire_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Fire do 
+  it 'exists and has attributes' do
+     data = {
+      "_id": "5cf5679a915ecad153ab68fd",
+      "allies": [
+                  "Ozai"
+                ],
+      "enemies": [
+                  "Earth Kingdom"
+                  ],
+      "name": "Chan (Fire Nation admiral)",
+      "affiliation": "Fire Nation Navy"
+  }
+    character = Fire.new(data)
+
+    expect(character.allies).to eq('Ozai')
+    expect(character.name).to eq('Chan (Fire Nation Admiral}')
+    expect(character.affiliation).to eq('Fire Nation Navy')
+    expect(character.enemies).to eq('Earth Kingdom')
+
+  end
+end

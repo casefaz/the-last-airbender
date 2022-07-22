9618eb9828aaa4da536d9a50c135db88e48b57f8
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-22 09:36:33 -0600

    Service finished

--- spec/services/fire_bender_spec.rb
+++ spec/services/fire_bender_spec.rb
@@ -4,12 +4,11 @@ RSpec.describe FireBenderService do
   it 'returns the data from the API' do 
     fire_nation = FireBenderService.get_nation_info('Fire Nation')
     # binding.pry
-    data = fire_nation[0].each do |fire|
-      expect(fire_nation[0]).to have_key([:allies])
-      expect(fire_nation[0]).to have_key([:enemies])
-      expect(fire_nation[0]).to have_key([:name])
-      expect(fire_nation[0]).to have_key([:affiliation])
-      expect(fire_nation[0]).to have_key([:photoUrl])
-    end
+    expect(fire_nation).to be_an(Array)
+    character_info = fire_nation.first
+    expect(character_info).to be_a(Hash)
+    expect(character_info).to have_key(:name)
+    expect(character_info).to have_key(:enemies)
+    expect(character_info).to have_key(:affiliation)
   end
 end
